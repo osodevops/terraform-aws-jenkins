55a83c0c8cb1f6bf417185393b1c8787ed79717e
Richard Gutkowski <richard@osodevops.io>
2020-03-31 17:39:59 +0100

    created a dedicated service and task definition for the jenkins slave

--- main.tf
+++ main.tf
@@ -17,6 +17,10 @@ locals {
   jenkins_fargate_cpu_value          = 2048 # 2 vCPU  - https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
   jenkins_fargate_memory_value       = 4096 # 4 GB    - https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
   jenkins_url                        = "https://${aws_route53_record.load-balancer.fqdn}"
+  jenkins_slave_cloudwatch_log_path  = "/ecs/service/${var.name_preffix}-jenkins-slave"
+  jenkins_slave_container_name       = "jenkins-slave"
+  jenkins_fargate_slave_cpu_value    = 512
+  jenkins_fargate_slave_memory_value = 1024
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
